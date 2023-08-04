6f6452d757568462da3337b47e688c56648b285f
krishnaaadba <krishnaaadba@gmail.com>
2023-08-04 13:18:20 -0700

    adding more functions to library

--- scripts/db_admin_lib.sh
+++ scripts/db_admin_lib.sh
@@ -28,3 +28,33 @@ kill_sleep_connections() {
     fi
 }
 
+show_master_status() {
+
+    [[ -z $1 ]] && echo "Error - Hostname is not provided" && return 1
+
+    local host=$1
+    local log_bin=`$MYDB -h$host -ANBe"select @@log_bin"`
+
+    if [[ $log_bin -ne 1 ]];then
+        echo "Binary logging is not enabled"
+        return 0
+    else
+        $MYDB -h$host -e"show master status;"
+    fi
+}
+
+show_slave_status() {
+
+    [[ -z $1 ]] && echo "Error - Hostname is not provided" && return 1
+
+    local host=$1
+    local slave_running=`$MYDB -h$host -ANBe"show status like 'Slave_running'"`
+
+    if [[ $slave_running == 'ON' ]];then
+        $MYDB -h$host -e"show slave status\G" | egrep -w 'Master_Host|Slave_IO_Running|Slave_SQL_Running|Seconds_Behind_Master|' 
+        return $?
+    else
+        echo "Not configured as a slave"
+        return 1
+    fi
+}
